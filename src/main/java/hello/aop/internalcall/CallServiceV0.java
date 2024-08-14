bbf384d36ef4f33e9120cc4b1e5973083cae416b
KOHYEONJEONG <080821@naver.com>
2024-08-15 00:54:35 +0900

    프록시 문제 해결 대안1(setter 의존 주입)

--- src/main/java/hello/aop/internalcall/CallServiceV0.java
+++ src/main/java/hello/aop/internalcall/CallServiceV0.java
@@ -9,7 +9,16 @@ public class CallServiceV0 {
 
     public void external() {
         log.info("call external");
+        //흐름
+        // 클라이언트 -> 프록시 호출 -> 어드바이스 호출(external()) -> 실제 external 호출(externaml())-> 실제 internal 호출(internal())
         internal(); //내부 메서드 호출(this.internal())  -- 어드바이스가 호출되지 않는것을 볼 수 있다.
+
+        // 출력
+        // aop=void hello.aop.internalcall.CallServiceV0.external()
+        // call external
+        // call internal  <-- 실제 대상만 호출됐고, 프록시가 호출되지 않았다.
+
+        //어드바이스가 호출되려면? 대안1인 CallServiceV1을 보러가자.
     }
 
     public void internal() {
--- src/main/java/hello/aop/internalcall/CallServiceV1.java
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -10,6 +10,15 @@ public class CallServiceV1 {
 
     private CallServiceV1 callServiceV1;
 
+    /**
+     * 생성자에 주입 시에는 오류가 난다
+     * - 생성자 주입은 순환 사이클을 만들기 때문에 실패한다.
+     * - 자기자신이 만들어지지 않은 상태에서 의존주입을 받을 수 없기 떄문이다.
+     *
+     * 대안으로는
+     * setter를 통해 자기자신을 주입받게 하면된다.
+     *
+     * */
     @Autowired
     public void setCallServiceV1(CallServiceV1 callServiceV1) {
         this.callServiceV1 = callServiceV1;
@@ -17,7 +26,9 @@ public class CallServiceV1 {
 
     public void external() {
         log.info("call external");
-        callServiceV1.internal(); //외부 메서드 호출
+        //흐름
+        // 클라이언트 -> 프록시 호출 -> 어드바이스 호출(external()) -> 실제 external 호출(externaml()) -> 어드바이스 호출(internal()) -> 실제 internal 호출
+        callServiceV1.internal(); //외부 메서드 호출(의존관계 주입받은 프록시가 호출된 후 실제 대상이 호출된다.)
     }
 
     public void internal() {
